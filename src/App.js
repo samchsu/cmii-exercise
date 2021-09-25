38aa1304e71df3eacb2cadc11336239f11167077
samchsu <63684737+samchsu@users.noreply.github.com>
2021-09-25 16:24:44 -0700

    Ability to choose and import Excel Sheet

--- src/App.js
+++ src/App.js
@@ -1,7 +1,26 @@
 import logo from './logo.svg';
 import './App.css';
+import React, { useEffect, useState } from 'react';
+import {OutTable, ExcelRenderer} from 'react-excel-renderer';
 
 function App() {
+  const [cols, setCols] = useState([]);
+  const [rows, setRows] = useState([]);
+  var fileHandler = (event) => {
+    let fileObj = event.target.files[0];
+
+    //just pass the fileObj as parameter
+    ExcelRenderer(fileObj, (err, resp) => {
+      if(err){
+        console.log(err);            
+      }
+      else{
+        setCols(resp.cols);
+        setRows(resp.rows);
+      }
+    });               
+  }
+
   return (
     <div className="App">
       <header className="App-header">
@@ -9,15 +28,10 @@ function App() {
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <input type="file" onChange={fileHandler} style={{"padding":"10px"}} />
+
       </header>
+      <OutTable data={rows} columns={cols} tableClassName="ExcelTable2007" tableHeaderRowClass="heading" />
     </div>
   );
 }
