83f41ac20dbf62955bab00f7d74d9ca7a3040eeb
samchsu <63684737+samchsu@users.noreply.github.com>
2021-09-28 00:17:43 -0700

    Able to collect user selected milestones and generate JSON object

--- src/App.js
+++ src/App.js
@@ -5,7 +5,10 @@ import {OutTable, ExcelRenderer} from 'react-excel-renderer';
 
 function App() {
   const [cols, setCols] = useState([]);
-  const [rows, setRows] = useState([]);
+  const [rows, setRows] = useState([{}]);
+  const [selected, setSelected] = useState([[]]);
+  const [started, setStarted] = useState(false);
+  // var [counter, setCounter] = useState(0);
   var fileHandler = (event) => {
     let fileObj = event.target.files[0];
 
@@ -17,21 +20,51 @@ function App() {
       else{
         setCols(resp.cols);
         setRows(resp.rows);
+        console.log(resp.rows);
       }
     });               
+  }
+
+  async function storeMilestone(milestone, completion) {
+    // console.log(milestone);
+    // console.log(completion);
+    // console.log(counter);
+    // selected[counter][0] = milestone;
+    // selected[counter][1] = completion;
+    selected[milestone] = completion;
+    console.log(selected);
+    // setCounter(counter++);
+    // counter++;
+    setStarted(true);
+    completion = "selected";
+  }
+
+  async function sendExcel() {
+    console.log(selected);
   }
 
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <input type="file" onChange={fileHandler} style={{"padding":"10px"}} />
+        <p> Please choose an Excel Sheet to Import </p>
+          <input type="file" onChange={fileHandler} style={{"padding":"10px"}} />
+        <ul>
+          { rows ? rows.map((data, i)=> (
+            <li key={i}>
+                   <a onClick={() => storeMilestone(data[0], data[1])}> {data[0]} - {data[1]}</a> 
+            </li> 
+          ))
+          : ""}
+        </ul>
 
+        {console.log(selected)}
+
+      <ul>
+      { started ? <button onClick={() => sendExcel()}>Continue</button>
+          : ""}
+      </ul>
       </header>
-      <OutTable data={rows} columns={cols} tableClassName="ExcelTable2007" tableHeaderRowClass="heading" />
     </div>
   );
 }
