05279f97ece366ef75487ab9b76885eef492dcd3
samchsu <63684737+samchsu@users.noreply.github.com>
2021-09-29 16:51:07 -0700

    Able to use JSON Object as data for new Excel File generated in Flask

--- src/App.js
+++ src/App.js
@@ -6,9 +6,14 @@ import {OutTable, ExcelRenderer} from 'react-excel-renderer';
 function App() {
   const [cols, setCols] = useState([]);
   const [rows, setRows] = useState([{}]);
-  const [selected, setSelected] = useState([[]]);
+  const [permRows, setPermRows] = useState([{}]);
+  const [list, setList] = useState(rows);
+  const [selected, setSelected] = useState({});
   const [started, setStarted] = useState(false);
-  // var [counter, setCounter] = useState(0);
+  const [imported, setImported] = useState(false);
+  const [selectedRows, setSelectedRows] = useState([{}]);
+  const fs = require('fs');
+  var [counter, setCounter] = useState(1);
   var fileHandler = (event) => {
     let fileObj = event.target.files[0];
 
@@ -20,6 +25,8 @@ function App() {
       else{
         setCols(resp.cols);
         setRows(resp.rows);
+        setPermRows(resp.rows);
+        setImported(true);
         console.log(resp.rows);
       }
     });               
@@ -31,16 +38,61 @@ function App() {
     // console.log(counter);
     // selected[counter][0] = milestone;
     // selected[counter][1] = completion;
-    selected[milestone] = completion;
+    // selected[counter] = 
+    // {
+    //   date: completion,
+    //   milestone: milestone
+    // };
+    if (counter === 1)
+    {
+      selected["Selected milestone(s)"] = [
+      {
+        milestone: milestone
+      }]
+
+      selected["Selected completion date"] = [
+      {
+        date: completion
+      }]
+    }
+    else 
+    {
+      selected["Selected milestone(s)"].push({milestone: milestone})
+      selected["Selected completion date"].push({})
+    }
+    
     console.log(selected);
-    // setCounter(counter++);
-    // counter++;
+    setCounter(counter+1);
     setStarted(true);
+    const withoutSelected = rows.filter((data) => data[1] !== completion);
+    setRows(withoutSelected);
+    console.log(withoutSelected);
+    const withSelected = rows.filter((data) => data[1] == completion);
+    setSelectedRows(withSelected);
     completion = "selected";
   }
 
   async function sendExcel() {
     console.log(selected);
+  //   fetch("http://localhost:5000", {
+  //     method: "GET",
+  //     headers: {
+  //         "Content-Type": "application/json",
+  //         Accept: "application/json"
+  //     },
+  // })
+
+    var res = await fetch("http://localhost:5000/export", {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json"
+        },
+        body: JSON.stringify(selected)
+    })
+    .then(res => {
+      console.log(res);
+      
+    })
   }
 
   return (
@@ -50,13 +102,24 @@ function App() {
         <p> Please choose an Excel Sheet to Import </p>
           <input type="file" onChange={fileHandler} style={{"padding":"10px"}} />
         <ul>
-          { rows ? rows.map((data, i)=> (
+          { imported ? (<p>Please choose from the following milestones:</p>)
+          : ""}
+          { imported ? rows.map((data, i)=> (
             <li key={i}>
                    <a onClick={() => storeMilestone(data[0], data[1])}> {data[0]} - {data[1]}</a> 
             </li> 
           ))
           : ""}
         </ul>
+
+        {/* <ul>
+          { selectedRows ? selectedRows.map((data, i)=> (
+            <li key={i}>
+                    {data[0]} - {data[1]}
+            </li> 
+          ))
+          : ""}
+        </ul> */}
 
         {console.log(selected)}
 
